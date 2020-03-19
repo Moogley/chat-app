d4e0544ed1253802bddfacb97b75c6f00950c6ab
Shane Mosley <shanemosley14@gmail.com>
2020-03-19 00:36:26 -0400

    Beginning to work on passport and express, added client info for google oauth

---
+++ config/index.js
@@ -0,0 +1,8 @@
+const GOOGLE = {
+    clientID: '77916096337-0en5f7l6opkk16jn7oodpe69agl81pbu.apps.googleusercontent.com',
+    clientSecret: 'rga2xhDzruyXBgnC_BszDhhf'
+};
+
+module.exports = {
+    GOOGLE
+}
--- src/App.js
+++ src/App.js
@@ -1,20 +1,69 @@
 import React, { useState, useRef } from 'react';
 import Chat from './chat';
-import Login from './login';
+// import Login from './login';
 import './App.css';
+import { Strategy } from 'passport-local';
 
-function App() {
-  const [msgWindow, sendMsg] = useState([])
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const passport = require('passport')
+const GoogleStrategy = require('passport-google-oauth')
+const keys = require('../config')
 
-  const sendRef = useRef();
+let user = {}
+
+passport.serializeUser( fn: (user, cb) => {
+  cb(null, user)
+});
+
+passport.deserializeUser( fn: (user, cb) => {
+  cb(null, user)
+});
+
+passport.use(new GoogleStrategy({
+  clientID: keys.GOOGLE.clientID,
+  clientSecret: keys.GOOGLE.clientSecret,
+  callbackURL: "/auth/google/callback"
+}, (accessToken, refreshToken, profile, cb) => {
+  console.log(JSON.stringify(profile))
+  user = {...profile}
+  return cb(null, profile)
+}))
+
+
+
+app.use(cors());
+app.use(passport.initialize())
+
+app.get('/auth/google', passport.authenticate( strategy: 'google', options: {
+  scope: ['profile', 'email']
+} ));
+app.get('/auth/google/callback',
+        passport.authenticate(('google'),
+        options: (req, res) => {
+          res.redirect('/chat')
+        }))
+
+app.get('/user', (req, res) => {
+  console.log('getting user data..')
+  res.send(user)
+})
+
+app.get('/auth/logout', (req, res) => {
+  console.log('logging out..')
+  user = {}
+  res.redirect('/login')
+})
+
+function ChatApp() {
+  
   
  
-
   
-
-
   return (
-    <div className="App">
+    <div className="ChatApp">
+      {/* <ChatHeader /> */}
       <Chat />
       <input ref={sendRef} type="text" />
       <button>Send</button>
@@ -22,4 +71,4 @@ function App() {
   );
 }
 
-export default App;
+export default ChatApp;
